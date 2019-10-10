30a6fc8c7262c2a73ad9f83963e9374b58a35861
Alex Vega <alexvega880@Alexs-MacBook-Air.local>
2019-10-10 16:25:35 -0700

    finished MVP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from "react";
 import "./App.css";
 import ImgList from "./components/ImgList";
+import styled from 'styled-components';
 
 
 
 function App() {
-
+  const Wrapper = styled.section`
+  padding: .4em;
+  background: #9CADBA;
+`;
+  const Title = styled.h1`
+  color:#A1E8AF;
   
+  `;
+
+
+
   return (
-    <div className="App">
+    <Wrapper className="App">
       {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun !
       </p> */}
 
       
-      <h3>NASA - Astronomy Picture of the Day.</h3>
+      <Title>NASA - Astronomy Picture of the Day</Title>
 
       <ImgList/>
-    </div>
+    </Wrapper>
   );
 }
 
--- src/components/ImgList.js
+++ src/components/ImgList.js
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ImgCard from "./ImgCard";
+import styled from 'styled-components';
 
+export const PageTemplate = styled.div`
+  background: ${props => (props.primary ? "#FFFFFF" : "#3366CC")};
+  color: ${props => (props.primary ? "#FFFFFF" : "#000000")};
+  border: 0;
+  margin: 5px 10px;
+  transition: 0.5s ease-in;
+  border: ${props =>
+    props.primary ? "2px solid #000000" : "2px solid #2a2223"};
+  &:hover {
+    border: ${props =>
+      props.primary ? "2px solid #FFFFFF" : "2px solid #FFFFFF"};
+  }
+`;
 
 export default function ImgList() {
     // NOTE: The value given to useState() must be of the same type as your vale is expected to be
@@ -9,11 +23,12 @@ export default function ImgList() {
   
     useEffect(() => {
       axios
-        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .get(`https://api.nasa.gov/planetary/apod?api_key=HGL2qxd9AoohvODt6T5BLhTKYIFWK6HLWv8IWDbL
+        `)
         .then(response => {
           // console.log("Nasa Pictures", response.data);
           const imgs = response.data;
-          console.log("NASA image", imgs);
+          // console.log("NASA image", imgs);
           setImg(imgs);
         })
         .catch(error => {
@@ -21,16 +36,20 @@ export default function ImgList() {
         });
     }, []);
   
+    
+  
+
+
     return (
-      <div className="img">
-            <ImgCard
+      <PageTemplate >
+            <ImgCard 
               title={imgs.title}
               picture={imgs.url}
               date={imgs.date}
               description={imgs.explanation}
             />
 
-      </div>
+      </PageTemplate>
     );
   }
 
